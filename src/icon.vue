79aa065aedc392b72c354a5b2a9ff63ffc3586b2
xcsf <yinchy@szmedi.com.cn>
2019-01-27 21:30:42 +0800

    xcsf: create commpent g-icon

---
+++ src/icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+<script>
+export default {
+props: {
+    name: {
+        type: String
+    }
+}
+};
+</script>
+<style lang="scss" scoped>
+.g-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
