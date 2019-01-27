cce1b21df863f4ecea2143796ca30e0ac8c2632c
xcsf <yinchy@szmedi.com.cn>
2019-01-27 16:34:05 +0800

    xcsf: icon & right icon position

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,24 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {};
+export default {
+  props: {
+    icon: {
+      type: String
+    },
+    iconPosition: {
+      type: String
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
 .g-button {
@@ -12,16 +28,37 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
-
   &:active {
     background-color: var(--button-active-bg);
   }
-
   &:focus {
     outline: none;
+  }
+  > .icon {
+    width: 1em;
+    height: 1em;
+    margin-right: .3em;
+    order: 1;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .3em; 
+    }
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
