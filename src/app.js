0466a84191d0330990e9f5272c41705d4c5d1840
xcsf <yinchy@szmedi.com.cn>
2019-02-15 17:24:24 +0800

    add input test case (TDD)测试驱动开发

--- src/app.js
+++ src/app.js
@@ -11,5 +11,10 @@ new Vue({
         loading1: false,
         loading2: true,
         loading3: false
+    },
+    methods:{
+        inputchange(e){
+            console.log(e);
+        }
     }
 })
---
+++ test/input.test.js
@@ -0,0 +1,78 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+//BDD 行为驱动测试 Mocha》》describe、it
+//chai》》expect。。。。
+describe('Input', () => {
+    it('存在.', () => {
+        expect(Input).to.be.ok
+    })
+    describe('props', () => {
+        const Constructor = Vue.extend(Input)
+        let vm
+        afterEach(() => {
+            vm.$destroy()
+        })
+        it('接受value', () => {
+            vm = new Constructor({
+                propsData: {
+                    value: '1234'
+                }
+            }).$mount()
+            const InputElement = vm.$el.querySelector('input')
+            expect(InputElement.value).to.equal('1234')
+        })
+
+        it('接受disabled', () => {
+            vm = new Constructor({
+                propsData: {
+                    disabled: true
+                }
+            }).$mount()
+            const InputElement = vm.$el.querySelector('input')
+            expect(InputElement.disabled).to.equal(true)
+        })
+        it('接受readonly', () => {
+            vm = new Constructor({
+                propsData: {
+                    readonly: true
+                }
+            }).$mount()
+            const InputElement = vm.$el.querySelector('input')
+            expect(InputElement.readOnly).to.equal(true)
+        })
+        it('接受error', () => {
+            vm = new Constructor({
+                propsData: {
+                    error: '1234'
+                }
+            }).$mount()
+            const useElement = vm.$el.querySelector('use')
+            expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+            const errorMessage = vm.$el.querySelector('.errormessage')
+            expect(errorMessage.innerText).to.equal('1234')
+        })
+    })
+    describe('事件', () => {
+        const Constructor = Vue.extend(Input)
+        let vm
+        afterEach(() => {
+            vm.$destroy()
+        })
+        it('支持change/input/focus/blur 事件', () => {
+            ['change', 'input', 'focus', 'blur'].forEach(item => {
+                vm = new Constructor({}).$mount()
+                const callback = sinon.fake();
+                vm.$on(item, callback)
+                let event = new Event(item);
+                let InputElement = vm.$el.querySelector('input')
+                InputElement.dispatchEvent(event);
+                expect(callback).to.have.been.calledWith(event)
+            })
+        })
+    })
+})
