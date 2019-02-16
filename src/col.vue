ca05378363e2a89a5c3a20c4c190cbd99f17ee8e
xcsf <yinchy@szmedi.com.cn>
2019-02-16 16:34:52 +0800

    junior row and col

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col" :class="{[`col-${span}`]:true}">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+    name:'Gulu-Col',
+    props: {
+        span: [Number,String]
+    }
+}
+</script>
+<style lang="scss" scoped>
+.col {
+  height: 100px;
+  background-color: gray;
+  width: 50%;
+  border: 1px solid red;
+  //col-n
+  $class: col-;
+  @for $n from 1 through 24 {
+    &.#{$class}#{$n} {
+      width: ($n/24) * 100%;
+    }
+  }
+}
+</style>
