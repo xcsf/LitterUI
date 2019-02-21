0c3df5c8f5bbd71bb9d2e67f60e53164dcd8a5d0
xcsf <yinchy@szmedi.com.cn>
2019-02-21 09:36:20 +0800

    animation

--- src/sider.vue
+++ src/sider.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="show">
+      <slot></slot>
+      <button @click="closeSider">close</button>
+    </div>
+  </transition>
 </template>
 <script>
 export default {
   name: "GuluSider",
+  data() {
+    return {
+      show: true
+    };
+  },
+  methods: {
+    closeSider() {
+      this.show = false;
+    }
+  },
   mounted() {
     // console.log("sider mounted");
   },
@@ -17,5 +30,19 @@ export default {
 
 <style lang="scss" scoped>
 .sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s;
+}
+.slide-enter,
+.slide-leave-to {
+  margin-left: -100px;
 }
 </style>
