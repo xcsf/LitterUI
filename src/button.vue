a68ce85b587d75cc849be44dbd8d49d6ec08d685
xcsf <jiaowofeizi@126.com>
2019-03-09 16:06:35 +0800

    去掉css变量

--- src/button.vue
+++ src/button.vue
@@ -8,9 +8,9 @@
   </button>
 </template>
 <script>
-import Icon from "./icon"
+import Icon from "./icon";
 export default {
-  name:'GuluButton',
+  name: "GuluButton",
   components: {
     "g-icon": Icon
   },
@@ -33,26 +33,37 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$button-height: 32px;
+$font-size: 14px;
+$button-bg: white;
+$button-active-bg: #eee;
+$border-radius: 4px;
+$border-color: #999;
+$border-color-hover: #666;
 @keyframes spin {
-  0%{ transform: rotate(0deg)}
-  100%{ transform: rotate(360deg)}
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
+  font-size: $font-size;
+  height: $button-height;
   padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
   display: inline-flex;
   justify-content: center;
   align-items: center;
   vertical-align: middle;
   &:hover {
-    border-color: var(--border-color-hover);
+    border-color: $border-color-hover;
   }
   &:active {
-    background-color: var(--button-active-bg);
+    background-color: $button-active-bg;
   }
   &:focus {
     outline: none;
