ec3df34b6e033f3598430a5f9b9846c31dcd80ea
xcsf <jiaowofeizi@126.com>
2019-06-19 16:35:28 +0800

    add table colnum

---
+++ src/table/table-column.vue
@@ -0,0 +1,21 @@
+<template></template>
+<script>
+export default {
+  name: "g-table-column",
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    field: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: Number
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
